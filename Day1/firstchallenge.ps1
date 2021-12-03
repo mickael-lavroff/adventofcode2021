5a74cbb69c89130f388869e9f44178482f474dbb
Mickael Lavroff <mickael.lavroff@gmail.com>
2021-12-03 16:29:16 -0500

    change variable names

--- Day1/firstchallenge.ps1
+++ Day1/firstchallenge.ps1
@@ -1,10 +1,10 @@
 $values = (Get-Content Day1\input.txt)
 $counter = 1
 
-for ($i = 1; $i -lt $yolo.Count ; $i++) { 
+for ($i = 1; $i -lt $values.Count ; $i++) { 
     if ($values[$i] -gt $values[$i - 1]) {
         $counter++ 
     } 
 }
 
-Write-Output "Increase : $counter / Decrease : $($yolo.Count - $counter)"
+Write-Output "Increase : $counter / Decrease : $($values.Count - $counter)"
